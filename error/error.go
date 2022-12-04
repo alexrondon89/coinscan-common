798988f26e2b3269daec60af5425a7a1ffe027d6
crondonveritone <crondon@veritone.com>
2022-12-04 17:49:39 -0300

    error folder added

---
+++ error/error.go
@@ -0,0 +1,42 @@
+package error
+
+type Error interface {
+	Error() string
+	StatusCode() int
+	InternalCode() string
+	Type() ErrorType
+}
+
+func New(errorType ErrorType, originalError error) Error {
+	return Err{
+		ErrorType:     errorType,
+		OriginalError: originalError,
+	}
+}
+
+type Err struct {
+	OriginalError error
+	ErrorType     ErrorType
+}
+
+type ErrorType struct {
+	Message      string `json:"message"`
+	InternalCode string `json:"codeError"`
+	HttpCode     int    `json:"codeHttp"`
+}
+
+func (err Err) Error() string {
+	return err.ErrorType.Message
+}
+
+func (err Err) StatusCode() int {
+	return err.ErrorType.HttpCode
+}
+
+func (err Err) InternalCode() string {
+	return err.ErrorType.InternalCode
+}
+
+func (err Err) Type() ErrorType {
+	return err.ErrorType
+}
