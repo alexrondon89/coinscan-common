dd5230982d21b14d6e75316112759b0fdef941a0
crondonveritone <crondon@veritone.com>
2022-12-09 11:31:18 -0300

    adding ping request to redis constructor

--- redis/redis.go
+++ redis/redis.go
@@ -2,6 +2,8 @@ package redis
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"github.com/go-redis/redis/v8"
 	"time"
 )
@@ -15,19 +17,22 @@ type redisCli struct {
 	conn *redis.Client
 }
 
-func New(address, password string, db int) RedisIntf {
+func New(host, port, password string, db int) (RedisIntf, error) {
+	address := fmt.Sprintf("%s:%s", host, port)
 	rdb := redis.NewClient(&redis.Options{
-		Addr:        address,
-		Password:    password, // no password set
-		DB:          db,       // use default DB
-		ReadTimeout: 3 * time.Second,
-		IdleTimeout: 5 * time.Minute,
-		MaxRetries:  3,
+		Addr:     address,
+		Password: password, // no password set
+		DB:       db,       // use default DB
 	})
+
+	_, err := rdb.Ping(context.Background()).Result()
+	if err != nil {
+		return nil, errors.New("error creating redis client: " + err.Error())
+	}
 
 	return redisCli{
 		conn: rdb,
-	}
+	}, nil
 }
 
 func (r redisCli) SetItem(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
